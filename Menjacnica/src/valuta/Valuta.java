320054fffc23ec10a21990a269d95fe3fadcced1
Jelena-S <jsreckovic1@gmail.com>
2020-03-02 10:25:28 +0100

    Dodate equals, hashcode i toString metode u klase Valuta i Kurs

--- Menjacnica/src/valuta/Valuta.java
+++ Menjacnica/src/valuta/Valuta.java
@@ -28,6 +28,46 @@ public class Valuta {
 	public void setKurseviNaDatum(List<Kurs> kurseviNaDatum) {
 		this.kurseviNaDatum = kurseviNaDatum;
 	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((kurseviNaDatum == null) ? 0 : kurseviNaDatum.hashCode());
+		result = prime * result + ((naziv == null) ? 0 : naziv.hashCode());
+		result = prime * result + ((skraceniNaziv == null) ? 0 : skraceniNaziv.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Valuta other = (Valuta) obj;
+		if (kurseviNaDatum == null) {
+			if (other.kurseviNaDatum != null)
+				return false;
+		} else if (!kurseviNaDatum.equals(other.kurseviNaDatum))
+			return false;
+		if (naziv == null) {
+			if (other.naziv != null)
+				return false;
+		} else if (!naziv.equals(other.naziv))
+			return false;
+		if (skraceniNaziv == null) {
+			if (other.skraceniNaziv != null)
+				return false;
+		} else if (!skraceniNaziv.equals(other.skraceniNaziv))
+			return false;
+		return true;
+	}
+	@Override
+	public String toString() {
+		return "Valuta [naziv=" + naziv + ", skraceniNaziv=" + skraceniNaziv + ", kurseviNaDatum=" + kurseviNaDatum
+				+ "]";
+	}
 	
 	
 }
