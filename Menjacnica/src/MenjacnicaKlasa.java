dea0ca941584c6a7b1f2ae59d0ff4cd757fba8f0
Jelena-S <jsreckovic1@gmail.com>
2020-03-02 10:48:31 +0100

    Dodata klasa MenjacnicaKlasa koja implementira InterfejsMenjacnica

---
+++ Menjacnica/src/MenjacnicaKlasa.java
@@ -0,0 +1,27 @@
+import java.util.Date;
+
+import interfejs.InterfejsMenjacnica;
+import kurs.Kurs;
+import valuta.Valuta;
+
+public class MenjacnicaKlasa implements InterfejsMenjacnica {
+
+	@Override
+	public void dodavanjeKursa(Kurs kurs, Date datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void brisanjeKursa(Date datum) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Kurs vratiKursNaDan(Valuta valuta, Date datum) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
