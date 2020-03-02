e75823b867b970b4579463722b295c4acb9bb016
Jelena-S <jsreckovic1@gmail.com>
2020-03-02 11:19:50 +0100

    Implementirana metoda dodavanjeKursa

--- Menjacnica/src/MenjacnicaKlasa.java
+++ Menjacnica/src/MenjacnicaKlasa.java
@@ -1,14 +1,18 @@
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import interfejs.InterfejsMenjacnica;
 import kurs.Kurs;
 import valuta.Valuta;
 
 public class MenjacnicaKlasa implements InterfejsMenjacnica {
+	
+	List<Valuta> valute = new ArrayList<>();
 
 	@Override
 	public void dodavanjeKursa(Kurs kurs, Date datum, Valuta valuta) {
-		// TODO Auto-generated method stub
+		//ja sam drugacija metoda
 
 	}
 
